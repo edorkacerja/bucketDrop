392edf8616c89ab303d324d0a4e548b27469dfc0
Edor Kacerja <edorkacerja@gmail.com>
2016-11-06 16:24:45 +0200

    glide library config

--- app/src/main/java/com/example/acerpc/bucketdrop/MainActivity.java
+++ app/src/main/java/com/example/acerpc/bucketdrop/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.acerpc.bucketdrop;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
-
+    Toolbar toolbar;
+    ImageView backgroundImage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        backgroundImage = (ImageView) findViewById(R.id.background_pic);
+        initBackgroundImg();
+    }
+
+    private void initBackgroundImg(){
+        Glide.with(this).load(R.drawable.background).into(backgroundImage);
     }
 }
